725ba8f9eee55a64963419fdb316ba4a5a349382
Prankk <cristianezequiel.bonilla@gmail.com>
2022-09-04 02:08:09 -0300

    Avance con BackEnd y FrontEnd, conseguí configurar el Login en el BackEnd y pasarlo al FrontEnd.

--- PortfolioPrankk/portfolio-BackEnd/src/main/java/com/portfolio/cb/Controller/PersonaController.java
+++ PortfolioPrankk/portfolio-BackEnd/src/main/java/com/portfolio/cb/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.cb.Entity.Persona;
 import com.portfolio.cb.Interface.IPersonaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -24,26 +25,26 @@ public class PersonaController {
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
-
-
-    @DeleteMapping("/personas/borrar/(id)")
+    
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
         return "La persona fue eliminada correctamente";
     }
     
-    // URL:PUERTO/personas/editar/4?nombre & apellido & img 
-    
-    @PutMapping("/personas/editar/(id)")
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
-                              @RequestParam("nombre") String nuevoNombre,
-                              @RequestParam("apellido") String nuevoApellido,
-                              @RequestParam("img") String nuevoImg){
+                               @RequestParam("nombre") String nuevoNombre,
+                               @RequestParam("apellido") String nuevoApellido,
+                               @RequestParam("img") String nuevoImg){
         Persona persona = ipersonaService.findPersona(id);
         
         persona.setNombre(nuevoNombre);
@@ -54,14 +55,13 @@ public class PersonaController {
         return persona;
     }
     
-    @GetMapping("/personas/traer/perfil")
+    @GetMapping("personas/traer/perfil")
     public Persona findPersona(){
         return ipersonaService.findPersona((long)1);
     }
-
-
-
+   
 }
+
                               
 
 
--- PortfolioPrankk/portfolio-FrontEnd/src/app/app.module.ts
+++ PortfolioPrankk/portfolio-FrontEnd/src/app/app.module.ts
@@ -13,6 +13,10 @@ import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -25,15 +29,20 @@ import { HttpClientModule } from '@angular/common/http';
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
